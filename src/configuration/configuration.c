598ab8473faa7f4c1797d89f024f88cdaeb40d12
camille.chaumont <camille.chaumont@telecomnancy.eu>
2026-04-30 15:40:24 +0200

    ajout du nbr de tours et des campements barbares

--- src/configuration/configuration.c
+++ src/configuration/configuration.c
@@ -3,9 +3,24 @@
 
 #include "configuration.h"
 
-void initialiser_configuration(Configuration *config, int width, int height, unsigned int seed, int sdl_mode) {
+Configuration * create_configuration(int width, int height, unsigned int seed,int nbr_tours, int nbr_barbares, int sdl_mode) {
+
+    Configuration * config = malloc(sizeof(Configuration));
+
+    if(config == NULL){
+        return NULL;
+    }
+
     config->w = width;
     config->h = height;
     config->s = seed;
+    config->t = nbr_tours;
+    config->b = nbr_barbares;
     config->is_sdl = sdl_mode;
+
+    return config;
+}
+
+void destroy_configuration(Configuration * config){
+    free(config);
 }
--- src/configuration/configuration.h
+++ src/configuration/configuration.h
@@ -5,10 +5,13 @@ typedef struct {
     int w;          //largeur
     int h;          // hauteur
     unsigned int s; // graine (entier forcément positif)
+    int t;
+    int b;
     int is_sdl;     // 1 si mode SDL, 0 si mode CLI
 } Configuration;
 
 // fonction permettant d'initialiser la configuration du jeu
-void initialiser_configuration(Configuration *config, int width, int height, unsigned int seed, int sdl_mode);
+Configuration * create_configuration(int width, int height, unsigned int seed,int nbr_tours,int nbr_barabares, int sdl_mode);
+void destroy_configuration(Configuration * config);
 
 #endif
