09ef22a45be13e3f7eceb3f5cd84bd4cdbc71408
LEFEBVRE Nathan <nathan.lefebvre@telecomnancy.eu>
2026-05-28 23:50:37 +0200

    remise food par défaut

--- src/game/game.c
+++ src/game/game.c
@@ -407,7 +407,7 @@ void give_bonus_building(Game* game, City* city, Building* building) {
 void give_bonus_tile(Game* game, City* city, Tile* tile) {
     if (game != NULL && city != NULL && tile != NULL) {
         switch(tile->biome) {
-            case 'P': city->new_ressources->ressource1 += 1;        //Food
+            case 'P': city->new_ressources->ressource1 += 2;        //Food
                 city->new_ressources->ressource2 += 1; break;       //Prod   
 
             case 'E': city->new_ressources->ressource1 += 1;        //Food
